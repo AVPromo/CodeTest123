2b33c2692360516a36b15d15f8b740fce2c6f0ad
Serhio P <115337355+AVPromo@users.noreply.github.com>
2023-08-14 01:59:02 +0500

    Add Project files

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arrIN = new string[4] {"hello", "2", "world", ":-)"};
+string[] arrOUT = new string[arrIN.Length];
+
+void SecondArrayWithIF(string[] arrIN, string[] arrOUT, int limit)
+{
+    int count = 0;
+    for (int i = 0; i < arrIN.Length; i++)
+    {
+    if(arrIN[i].Length <= limit)
+        {
+        arrOUT[count] = arrIN[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArrayWithIF(arrIN, arrOUT, 3);
+PrintArray(arrOUT);
